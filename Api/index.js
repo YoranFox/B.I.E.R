4913f46f64693d24c99cfa87379aeb9e9c572101
Yoran de Vos <yorandevos12@gmail.com>
2021-07-11 15:03:15 +0200

    Added logger to api

--- Api/index.js
+++ Api/index.js
@@ -6,6 +6,31 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080;  
 const router = express.Router();
 
+let demoLogger = (req, res, next) => {
+    let current_datetime = new Date();
+    let formatted_date =
+      current_datetime.getFullYear() +
+      "-" +
+      (current_datetime.getMonth() + 1) +
+      "-" +
+      current_datetime.getDate() +
+      " " +
+      current_datetime.getHours() +
+      ":" +
+      current_datetime.getMinutes() +
+      ":" +
+      current_datetime.getSeconds();
+    let method = req.method;
+    let url = req.url;
+    let status = res.statusCode;
+    let log = `[${formatted_date}] ${method}:${url} ${status}`;
+    console.log(log);
+    next();
+  };
+
+
+  router.use(demoLogger)
+
 
 // Status of the API
 router.get('/status', function(req, res) {
