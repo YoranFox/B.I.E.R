ac10b317a154c323c56851f1f5eb958c3a8df11c
Yoran de Vos <Yoran.de.vos@dockbite.nl>
2022-05-19 23:02:52 +0200

    Added enums

--- robot_script/AI/main_ai.py
+++ robot_script/AI/main_ai.py
@@ -1,49 +1,68 @@
+from enum import Enum
+
+
+class RobotState(Enum):
+    IDLE = 0,
+    ACTION = 1,
+    OFF = 2,
+    ERROR = 3
+
+
+class ActionType(Enum):
+    MOVING = 0,
+
+
+robot_state = RobotState.OFF
+
+
 def main_loop():
 
-    # state machine for actions
+    while 1:
 
-    # IDLE
+        # state machine for actions
+
+        # IDLE
 
         # check api state for new action
 
         # if action -> generate action list
 
-            # do actions
+        # do actions
 
-    # ACTION
+        # ACTION
 
         # MOVING
 
-            # calculate latest location with all data we have
+        # calculate latest location with all data we have
 
-            # update path to take
+        # update path to take
 
-            # update arduino state
+        # update arduino state
 
         # WAIT
 
-            # check if wait function is ok
+        # check if wait function is ok
 
-            # if ok -> do next action
+        # if ok -> do next action
 
         # LOAD
 
-            # Beverage loading on robot (custom logic)
-        
+        # Beverage loading on robot (custom logic)
+
         # CHARGE
 
-            # charging logic
+        # charging logic
 
-    # OFF (charging, chosen that the robot is off)
+        # OFF (charging, chosen that the robot is off)
 
         # first loop shut down battery eating functions and modules
 
-    # ERROR
+        # ERROR
 
         # unable to handle error or exception
 
         # needs manual reset
 
-    # check api state for interupts
+        # check api state for interupts
 
         # api call every 5 seconds probably in seperate process to update api state
